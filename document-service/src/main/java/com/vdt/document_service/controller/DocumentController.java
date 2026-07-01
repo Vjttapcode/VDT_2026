dcceae05e159539f6056d4fabae2bf72f59ff697
Vjttapcode <ducvn180@gmail.com>
2026-07-01 16:32:56 +0700

    feat(document): add dashboard stats service & endpoints

--- document-service/src/main/java/com/vdt/document_service/controller/DocumentController.java
+++ document-service/src/main/java/com/vdt/document_service/controller/DocumentController.java
@@ -1,15 +1,29 @@
 package com.vdt.document_service.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.vdt.document_service.dto.DashboardStatsDto;
 import com.vdt.document_service.dto.DocumentRequest;
 import com.vdt.document_service.dto.DocumentResponse;
-import com.vdt.document_service.service.DocumentService;
 import com.vdt.document_service.dto.RejectRequest;
 import com.vdt.document_service.dto.RenewRequest;
+import com.vdt.document_service.service.DocumentService;
+
 import jakarta.validation.Valid;
-import java.util.List;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequestMapping("/documents")
@@ -60,4 +74,10 @@ public class DocumentController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) { service.delete(id); }
+
+    @GetMapping("/dashboard/stats")
+    public DashboardStatsDto dashboardStats() {
+        return service.dashboardStats();
+    }
+    
 }
